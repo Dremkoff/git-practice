c43b9d1e6f46181286c4aacff43cb9e7bbb59600
ndremkoff <n.dremkoff@gmail.com>
2026-06-13 09:26:56 +0300

    chore: add pg dotenv

---
+++ src/database/client.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+export const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-const main = () => console.log("Hello World");
+const { pool } = require("./database/client");
+
+const main = () => {
+  pool.query("SELECT NOW()", (err, res) => {
+    if (err) {
+      console.error("Connection error", err.stack);
+    } else {
+      console.log("Connected to PostgreSQL. Time:", res.rows[0].now);
+    }
+  });
+};
 
 main();
